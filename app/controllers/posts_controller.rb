f114db67821802f82a8271a0a840e62424fed6c4
Ada Wan <awan9796@gmail.com>
2016-01-09 13:04:33 -0500

    oops - changed the file before switching branches

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,16 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.all
-    
+    @posts.each_with_index do |posts, index|
+      if index == 5
+        post.title "SPAM"
+      end
+
+      elsif index % 5 == 0
+        post.title "SPAM"
+      end
+
+    end
   end
 end
 
