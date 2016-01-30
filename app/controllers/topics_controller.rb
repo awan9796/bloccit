b2e9be8806c68a29d9c260c5cae35f53a78f88a1
Ada Wan <awan9796@gmail.com>
2016-01-30 13:59:34 -0500

    CP35_assignment REDO

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -14,15 +14,20 @@ class TopicsController < ApplicationController
 
   def create
 
-     @topic.public = params[:topic][:public]
+    @topic = Topic.new
+    @topic.name = params[:topic][:name]
+    @topic.description = params[:topic][:description]
+    @topic.public = params[:topic][:public]
 
-     if @topic.save
-       redirect_to @topic, notice: "Topic was saved successfully."
-     else
-       flash.now[:alert] = "Error creating topic. Please try again."
-       render :new
-     end
-   end
+    if @topic.save
+      redirect_to @topic, notice: "Topic was saved successfully."
+    else
+      flash.now[:alert] = "Error creating topic. Please try again."
+      render :new
+    end
+  end
+
+
 
    def edit
      @topic = Topic.find(params[:id])
--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,3 +1,8 @@
 class Topic < ActiveRecord::Base
   has_many :posts, dependent: :destroy
+
+  validates :name, length: { minimum: 5 }
+  validates :name, presence: true
+  validates :description, length: { minimum: 25}
+  validates :description, presence: true
 end
