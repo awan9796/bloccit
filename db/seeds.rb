2c5097b7f78c328ac192a7de958a2cbd08f0b8c5
Ada Wan <awan9796@gmail.com>
2016-02-07 18:12:43 -0500

    CP40

--- db/seeds.rb
+++ db/seeds.rb
@@ -45,11 +45,19 @@ posts = Post.all
   )
 end
 
-user = User.first
-user.update_attributes!(
-  email: 'awan9796@hotmail.com',
-  password: 'Password1'
-)
+admin = User.create!(
+   name:     'Admin User',
+   email:    'admin@example.com',
+   password: 'helloworld',
+   role:     'admin'
+ )
+
+
+ member = User.create!(
+   name:     'Member User',
+   email:    'member@example.com',
+   password: 'helloworld'
+ )
 
 
 puts "Seed finished"
