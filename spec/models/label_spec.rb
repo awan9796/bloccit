ecfb30c8402836edbea4f0a501105e817b62cfdb
Ada Wan <awan9796@gmail.com>
2016-02-16 18:24:24 -0500

    CP42 Labels FIXED

--- spec/models/label_spec.rb
+++ spec/models/label_spec.rb
@@ -1,18 +1,18 @@
 require 'rails_helper'
 
- RSpec.describe Label, type: :model do
+RSpec.describe Label, type: :model do
 
-   let(:topic) { Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph) }
-   let(:user) { User.create!(name: "Bloccit User", email: "user@bloccit.com", password: "helloworld") }
-   let(:post) { topic.posts.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph, user: user) }
-   let(:label) { Label.create!(name: 'Label') }
-   let(:label2) { Label.create!(name: 'Label2') }
+  let(:topic) { Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph) }
+  let(:user) { User.create!(name: "Bloccit User", email: "user@bloccit.com", password: "helloworld") }
+  let(:post) { topic.posts.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph, user: user) }
+  let(:label) { Label.create!(name: 'Label') }
+  let(:label2) { Label.create!(name: 'Label2') }
 
-   it { is_expected.to have_many :labelings }
-   it { is_expected.to have_many(:topics).through(:labelings) }
-   it { is_expected.to have_many(:posts).through(:labelings) }
+  it { is_expected.to have_many :labelings }
+  it { is_expected.to have_many(:topics).through(:labelings) }
+  it { is_expected.to have_many(:posts).through(:labelings) }
 
-   describe "labelings" do
+  describe "labelings" do
      it "allows the same label to be associated with a different topic and post" do
        topic.labels << label
        post.labels << label
@@ -32,4 +32,5 @@ require 'rails_helper'
         expect(Label.update_labels(labels)).to eq(labels_as_a)
       end
     end
- end
+
+end
