d55c3c8deada8e735e29bc50464d946f4145137a
Ada Wan <awan9796@gmail.com>
2016-02-17 13:09:58 -0500

    CP43_assignment

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -59,6 +59,7 @@ RSpec.describe Post, type: :model do
          expect( post.points ).to eq(@up_votes - @down_votes)
        end
      end
+   end
 
      describe "#update_rank" do
        it "calculates the correct rank" do
@@ -78,5 +79,21 @@ RSpec.describe Post, type: :model do
          expect(post.rank).to eq (old_rank - 1)
        end
      end
+  
+
+     describe "#create_vote" do
+       it "sets the post up_votes to 1" do
+         expect(post.up_votes).to eq(1)
+       end
+
+       it "calls #create_vote when a post is created" do
+         post = topic.posts.new(title: RandomData.random_sentence, body: RandomData.random_paragraph, user: user)
+         expect(post).to receive(:create_vote)
+         post.save
+       end
+
+       it "associates the vote with the user that posted it" do
+         expect(post.votes.first.user).to eq(post.user)
+       end
+     end
    end
-end
