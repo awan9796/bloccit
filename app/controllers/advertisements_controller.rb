096d46d5063999f47e279c05a71b7734ce63405a
Ada Wan <awan9796@gmail.com>
2016-03-27 14:09:13 -0400

    CP32_assignment_REV

---
+++ app/controllers/advertisements_controller.rb
@@ -0,0 +1,27 @@
+class AdvertisementsController < ApplicationController
+  def index
+    @advertisements = Advertisement.all
+  end
+
+  def show
+    @advertisement = Advertisement.find(params[:id])
+  end
+
+  def new
+    @advertisement = Advertisement.new
+  end
+
+  def create
+    @advertisement = Advertisement.new
+    @advertisement.title = params[:advertisement][:title]
+    @advertisement.copy = params[:advertisement][:copy]
+    @advertisement.price = params[:advertisement][:price]
+
+    if @advertisement.save
+      flash[:notice] = "Your Ad has been saved"
+    else
+      flash[:error] = "We couldn't save your Ad.  Please try again"
+      render :new
+    end
+  end
+end
