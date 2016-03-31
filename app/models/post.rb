2062e30466a4fee46a72676faf2396329b76e9dd
Ada Wan <awan9796@gmail.com>
2016-03-31 19:39:03 -0400

    CP44_assignment_REV

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,6 +6,7 @@ class Post < ActiveRecord::Base
   has_many :labels, through: :labelings
   has_many :votes, dependent: :destroy
   has_many :favourites, dependent: :destroy
+  after_create :create_favourite
 
   default_scope { order('rank DESC') }
 
@@ -30,7 +31,7 @@ class Post < ActiveRecord::Base
     age_in_days = (created_at - Time.new(1970,1,1)) / 1.day.seconds
     new_rank = points + age_in_days
     update_attribute(:rank, new_rank)
-  end
+   end
 
     def create_favourite
       Favourite.create(post: self, user: self.user)
