0ccc179becd5854bebb3635e314ed049b7cecff2
Ada Wan <awan9796@gmail.com>
2016-04-14 22:08:26 -0400

    CP30_assginment_FINALgit add db/seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -22,11 +22,11 @@ end
 
 
 puts "#{Post.count}"
-Post.find_or_create_by(title: "Ozzie-Apr14", body: "Ozzie is a big brown Rhodesian Ridgeback-Apr14")
+unique_post = Post.find_or_create_by(title: "Ozzie-Apr14", body: "Ozzie is a big brown Rhodesian Ridgeback-Apr14")
 puts "#{Post.count}"
 
 puts "Seed finished"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
 
-Comment.find_or_create_by(title: "Ozzie-Apr14")
+Comment.find_or_create_by(body: "abcdehahfjiewufi jhfjha fhajfh", post: unique_post)
